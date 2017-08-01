161a15457bd09341cb9aba9c5ed920e40a1c3434
kiingroup <30615980+kiingroup@users.noreply.github.com>
2017-08-01 14:12:48 +0800

    Update test1.js

--- test/test1.js
+++ test/test1.js
@@ -1,13 +1,9 @@
-const Telegraf = require('../')
-
-const bot = new Telegraf(339280148:AAG2s7Jjh6CQcWsj4ffGo2EllkwLLf0i2Q)
-
-// Set telegram webhook
-// npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://--------.localtunnel.me/secret-path')
-
-// Start https webhook
-// FYI: First non-file reply will be served via webhook response
-bot.startWebhook('/secret-path', null, 3000)
-
-bot.on('text', ({ reply }) => reply('Hey there!'))
+const Telegraf = require('telegraf')
+const app = new Telegraf(339280148:AAG2s7Jjh6CQcWsj4ffGo2EllkwLLf0i2Q)
+app.command('start', (ctx) => {
+  console.log('start', ctx.from)
+  ctx.reply('Welcome!')
+})
+app.hears('hi', (ctx) => ctx.reply('Hey there!'))
+app.on('sticker', (ctx) => ctx.reply('👍'))
+app.startPolling()
